d96403d240fdd8a8cf5556222422e98797640e8d
Vanessa Guerrero <vamarilug@gmail.com>
2020-06-24 15:27:36 -0700

    Add user interface
    Co-authored-by: Spencer Moody <spencer.moody@outlook.com>

--- src/main.js
+++ src/main.js
@@ -2,10 +2,21 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Date } from './weekdaycalculator.js';
+
+
+
 
 $(document).ready(function () {
-  // $('#submit-button').submit(function (event) {
-  //     event.preventDefault();
+  $('#input').submit(function (event) {
+      event.preventDefault();
+      let year = parseInt($("input#year").val());
+      let month = parseInt($("input#month").val());
+      let day = parseInt($("input#day").val());
 
-  // });
+      let calculate = new Date(year, month, day);
+      console.log("hello");
+      $("#showResult").html(calculate.myDate());
+
+  });
 });
